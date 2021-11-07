15cba7df98acc36983cd17f66e04873d0a85a25f
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2021-11-07 17:10:16 -0500

    Update template.py

--- template.py
+++ template.py
@@ -1,15 +1,16 @@
 import sys
-from collections import deque
-sys.setrecursionlimit(1000000)  # comment this out if you are getting memory error on pypy3
+import collections
+import bisect
+#sys.setrecursionlimit(1000000)  # comment this out if you are getting memory error on pypy3
 readline = lambda: sys.stdin.readline().strip()
 readint = lambda: int(sys.stdin.readline())
 printf = lambda x: sys.stdout.write(f"{x}\n")  # comment out when using py2
-prints = lambda x, y="\n": sys.stdout.write(x + y)
+prints = lambda x: sys.stdout.write(x)
 printline = lambda x: list(map(printf, x))
 printeach = lambda x, y="": print(*x, sep=y)  # x is iterable and y is separator, also comment out when using py2
 gi = lambda: list(map(int, readline().split()))
 gs = lambda: readline().strip().split()
-emp = lambda x, y: [x for _ in range(y)]  # only use for 1d arrays
+emp = lambda x, y: [x]*y  # only use for 1d arrays
 fill = lambda x: list(range(1, x+1))
 flat = lambda x, l: x.join(map(str, l))  # " \n"[i == n] also works
 max_int, min_int = float('inf'), float('-inf')
