d233dd8bf907bcb0b172e8bf5a8d63685c11d33b
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2021-11-21 18:32:23 -0500

    Update methods.py

--- methods.py
+++ methods.py
@@ -197,3 +197,21 @@ def fib(n):  # the a value is the nth fib number and MOD is 1e9+7
     if n%2 == 1: a, b, c = (a+b)%MOD, a, b
     return a, b, c
 #########################################################
+def cyclic():  # call this function to check for a cycle within a directed graph (returns 1 for cycle found)
+    vis = emp(0, n+1)
+    res = emp(0, n+1)
+    for i in range(n):
+        if not vis[i] and dfscycle(i, vis, res):
+            return 1
+    return 0
+
+def dfscycle(x, vis, res):
+    vis[x] = 1
+    res[x] = 1
+    for e in g[x]:
+        if not vis[e] and dfscycle(e, vis, res):
+            return 1
+        elif res[e]: return 1
+    res[x] = 0
+    return 0
+#########################################################
