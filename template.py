051402ce4472edb411fbf5d5e134a3a55061faa2
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2021-12-01 08:40:27 -0500

    Update template.py

--- template.py
+++ template.py
@@ -8,11 +8,11 @@ readint = lambda: int(sys.stdin.readline())
 printf = lambda x: sys.stdout.write("%d\n" % x)  # comment out when using py2
 prints = lambda x: sys.stdout.write(x)
 printline = lambda x: list(map(printf, x))
-printeach = lambda x, y="": print(*x, sep=y)  # x is iterable and y is separator, also comment out when using py2
+#printeach = lambda x, y="": print(*x, sep=y)  # x is iterable and y is separator, also comment out when using py2
 gi = lambda: list(map(int, readline().split()))
 gs = lambda: readline().strip().split()
 emp = lambda x, y: [x]*y  # only use for 1d arrays
 fill = lambda x: list(range(1, x+1))
 flat = lambda x, l: x.join(map(str, l))  # " \n"[i == n] also works
-max_int, min_int, MOD = float('inf'), float('-inf'), int(1e9+7)
+max_int, min_int, MOD = sys.maxsize, -sys.maxsize, int(1e9+7)
 ######################## End of Template #######################
