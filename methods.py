416677a3192b58fe6c32b0758a57a0e5b9aeed21
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2021-12-06 08:51:35 -0500

    Update methods.py

--- methods.py
+++ methods.py
@@ -259,3 +259,15 @@ def mst():  # 0 indexed, using adj-matrix
             if not vis[i] and dist[i] > g[v][i]: dist[v] = g[v][i]
     return sum(d)
 #########################################################
+# DSU #
+def findset(d):
+    if d != p[d]: p[d] = findset(p[d])
+    return p[d]
+
+def union(a, b):
+    a, b = findset(a), findset(b)
+    if a != b:
+        if s[a] < s[b]: a, b = b, a
+        p[b] = a
+        if s[a] == s[b]: s[a] += 1
+#########################################################
