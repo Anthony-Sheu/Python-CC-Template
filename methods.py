baa736d92e4540597b42ffe5d6c8ccd40a41dffd
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2021-12-06 13:34:40 -0500

    Update methods.py

--- methods.py
+++ methods.py
@@ -270,4 +270,14 @@ def union(a, b):
         if s[a] < s[b]: a, b = b, a
         p[b] = a
         if s[a] == s[b]: s[a] += 1
+            
+def union(a, b):
+    a, b = findset(a), findset(b)
+    if a == b: return
+    if s[a] >= s[b]:
+        s[a] += s[b]
+        p[b] = a
+    else:
+        s[b] += s[a]
+        p[a] = b
 #########################################################
