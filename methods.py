e9728268e81def69e8536e5f2fe085c261e99b69
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2021-12-26 23:26:28 -0500

    Update methods.py

--- methods.py
+++ methods.py
@@ -281,3 +281,16 @@ def union(a, b):
         s[b] += s[a]
         p[a] = b
 #########################################################
+def fib_matrix():
+    a, b = 0, 1
+    for _ in bin(readint()%2000000016)[2:]:
+        if _ == '1':
+            temp = (a*((2*b)-a))%MOD
+            b = (a*a+b*b)%MOD
+            a = temp
+            temp = b
+            b = (a+b)%MOD
+            a = temp
+        else: a, b = (a*(2*b-a))%MOD, (a*a+b*b)%MOD
+    printf(a)
+#########################################################
