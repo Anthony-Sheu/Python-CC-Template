421ba1ffbb3192e574c4139ac1dcb75a35b86914
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2022-02-10 12:19:41 -0500

    Create SegmentedSieve.py

---
+++ implementation/SegmentedSieve.py
@@ -0,0 +1,25 @@
+def Sieve(n):
+    prime = emp(1, n+1)  # 0 for composite, 1 for prime
+    i = 2
+    prime[0] = prime[1] = 0
+    while i*i <= n:
+        if prime[i]:
+            for _ in range(i*i, n+1, i): prime[_] = 0
+        i += 1
+    return prime
+
+def SegmentedSieve(n):
+    lim = int(__import__("math").sqrt(n)+1)
+    Sieve(lim)
+    lo, hi = lim, lim*2
+    while lo < n:
+        if hi >= n: hi = n
+        prime = emp(1, n+1)
+        for i in range(n):
+            lolim = int(__import__('math').floor(lo / prime[i])*prime[i])
+            if lolim < lo: lolim += prime[i]
+            for j in range(lolim, hi, prime[i]): prime[j-lo] = 0
+        for i in range(lo, hi):
+            if prime[i-lo]: print(i)
+        lo += lim
+        hi += lim
