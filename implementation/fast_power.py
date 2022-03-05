94f3bdc4c6098f82d3d1309958c865bdf02e3117
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2022-03-05 13:39:16 -0500

    Update fast_power.py

--- implementation/fast_power.py
+++ implementation/fast_power.py
@@ -14,3 +14,24 @@ def qpow(base, exp):
     x *= x
     if exp%2: x *= base
     return x
+#####################################
+def qpow(base, exp, mod):
+    if not base: return 0
+    if not exp: return 1
+    if not exp%2:
+        res = qpow(base, exp//2, mod)
+        res = (res * res) % mod
+    else:
+        res = base % mod
+        res = (res * qpow(base, exp-1, mod) % mod) % mod
+    return (res + mod) % mod
+#####################################
+def qpow(base, exp, mod):
+    res = 1
+    base %= mod
+    if not base: return 0
+    while exp > 0:
+        if exp & 1 == 1: res *= base % mod
+        exp >>= 1
+        base *= base % mod
+    return res
