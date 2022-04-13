0b7bbdc4242ba457700a5ecfa8815a6bee3de3a2
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2022-04-13 12:32:35 -0400

    Update Correlation Coefficient.py

--- math/Correlation Coefficient.py
+++ math/Correlation Coefficient.py
@@ -1,5 +1,4 @@
 from statistics import *
-from math import *
-x, y = gi(), gi()
+x, y = [list(map(int, input().split())) for i in range(2)]
 mx, my = mean(x), mean(y)
-print(sum([(x[i]-mx)*(y[i]-my) for i in range(len(x)])/sqrt(sum([(x[i]-mx)**2 for i in range(len(x)]) * sum([(y[i]-my)**2 for i in range(len(y)])))
+print(sum([(x[i] - mx) * (y[i] - my) for i in range(len(x))]) / __import__('math').sqrt(sum([(x[i] - mx) ** 2 for i in range(len(x))]) * sum([(y[i] - my) ** 2 for i in range(len(y))])))
