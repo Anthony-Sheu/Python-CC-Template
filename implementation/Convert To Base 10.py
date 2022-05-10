44592c6ad0c92007f6b9da8d52bb6c25be5bd8a8
Anthony-Sheu <87908851+Anthony-Sheu@users.noreply.github.com>
2022-05-10 14:54:58 -0400

    Update Convert To Base 10.py

--- implementation/Convert To Base 10.py
+++ implementation/Convert To Base 10.py
@@ -1,1 +1,1 @@
-def b10(base, num): return sum([int(num[i])*pow(base, len(num)-1-i) for i in range(len(num))])
+def b10(base, num): return sum([int(num[i])*pow(base, len(num)-1-i) for i in range(len(num))])  # num must be provided as a string
